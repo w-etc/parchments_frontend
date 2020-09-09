483d6739f8b98622f6a60f03194611bb8f03dd80
w-etc <luwolf7@gmail.com>
2020-09-09 00:31:22 -0300

    adding support of parentParchmentId in frontend

--- lib/models/parchment.dart
+++ lib/models/parchment.dart
@@ -1,14 +1,16 @@
 class Parchment {
+  final int parentParchmentId;
   final int id;
   final String title;
   final String contents;
   final List<Parchment> continuations;
 
-  Parchment({this.id, this.title, this.contents, this.continuations});
+  Parchment({this.parentParchmentId, this.id, this.title, this.contents, this.continuations});
 
   factory Parchment.fromJson(Map<String, dynamic> json) {
     final continuations = json['continuations'] as List;
     return Parchment(
+      parentParchmentId: json['parentParchment'] != null ? json['parentParchment.id'] : null,
       id: json['id'],
       title: json['title'],
       contents: json['contents'],
