bc03f1de83a67d00c5d2b1484b5f35673585c3c6
w-etc <luwolf7@gmail.com>
2020-10-03 15:09:42 -0300

    adding fallback text in case core parchments cannot be fetched

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:parchments_flutter/components/parchment_card/parchment_card_list.dart';
 import 'package:parchments_flutter/components/parchments_navigation_bar.dart';
 import 'package:parchments_flutter/components/search_bar.dart';
+import 'package:parchments_flutter/constants/fonts.dart';
 import 'package:parchments_flutter/models/parchment.dart';
 import 'package:parchments_flutter/services/http_service.dart';
 
@@ -32,8 +33,14 @@ class _HomePageState extends State<HomePage> {
             future: coreParchments,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Column(
-                    children: separatedParchmentCards(snapshot.data)
+                if (snapshot.data.length > 0) {
+                  return Column(
+                      children: separatedParchmentCards(snapshot.data)
+                  );
+                }
+                return Padding(
+                  padding: EdgeInsets.only(top: 200, left: 30, right: 30),
+                  child: Text('We\'re having trouble bringing the latest Parchments. Please try again later.', style: TextStyle(fontFamily: NOTO_SERIF, fontSize: 16.0), textAlign: TextAlign.center,)
                 );
               }
               return Container();
