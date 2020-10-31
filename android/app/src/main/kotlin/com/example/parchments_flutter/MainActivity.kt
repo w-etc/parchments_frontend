0550fe0639a100a086ce62d0693cb327243c6ba0
w-etc <luwolf7@gmail.com>
2020-10-31 15:57:48 -0300

    updating pubspec.yaml / recreating android and ios folders

--- android/app/src/main/kotlin/com/example/parchments_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.parchments_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/parchments_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parchments_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
